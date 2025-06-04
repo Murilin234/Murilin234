f689b9ae4b96404b56d85742787fc490e9e32342
Murilin234 <murilo.andrade.figueiredo@escola.pr.gov.br>
2025-06-04 14:28:08 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+Const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave () {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
+
+
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+    
+    retur palavras;
+}
+
+funcion contaFrenquencias(palavras){
+   const frequencias = contaFrenquencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra
+
+        function ordenaPalavra(p1, p2){
+            return frequencias[p2] - frequencias[p1]
+        }
+    );
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+    for(let i of palavras){
+        frequencias[i] = 0;
+        for (let j in palavras){
+            if (i == j){
+                frequencias[i]++;
+            }
+        }
+    }
+    
+   return frequencias;
+
+}
